0d26b0138d5e8cf48a892bcd17859fc4483957ab
pedrogiampietro <pedrogiampietro@hotmail.com>
2022-11-21 16:15:10 -0300

    add: Finish first project @Rocketseat

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="center">
+    <a href="https://pt-br.reactjs.org/"> Imhere</a>
+</h1>
+<p align="center">🚀 Projeto 01 do curso da rocketseat - trilha 2022.
+App para cadastro de participantes em eventos
+</p>
+
+<h1 align="center">
+  <img alt="NextLevelWeek" title="#NextLevelWeek" src="./screenshots/screen_1.jpeg" width="250" height="524"/>
+  <img alt="NextLevelWeek" title="#NextLevelWeek" src="./screenshots/screen_2.jpeg" width="250" height="524"/>
+</h1>
+
+### Features
+
+- [x] FlatList
+- [x] Alert
+- [x] Text
+- [x] StyleSheet
+- [x] TouchableOpacity
+
+### 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- [Expo](https://expo.io/)
+- [Node.js](https://nodejs.org/en/)
+- [React](https://pt-br.reactjs.org/)
+- [React Native](https://reactnative.dev/)
+- [TypeScript](https://www.typescriptlang.org/)
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,81 @@
+import {
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity,
+	FlatList,
+	Alert,
+} from 'react-native'
+import React, { useState } from 'react'
+import { styles } from './styles'
+import { Participants } from '../../components/participants'
+
+export function Home() {
+	const [participants, setParticipants] = useState<string[]>([])
+	const [participantsName, setParticipantsName] = useState('')
+
+	function handleParticipantAdd() {
+		if (participants.includes(participantsName.trim())) {
+			return Alert.alert('Aviso', 'Já exite um participante com esse nome')
+		}
+
+		setParticipants((prevState) => [...prevState, participantsName.trim()])
+		setParticipantsName('')
+	}
+
+	function handleParticipantRemove(name: String) {
+		Alert.alert('Remover', `Deseja remover o participante ${name}`, [
+			{
+				text: 'Sim',
+				onPress: () =>
+					setParticipants((prevState) =>
+						prevState.filter((participant) => participant !== name)
+					),
+			},
+			{
+				text: 'Não',
+				style: 'cancel',
+			},
+		])
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.eventName}>Nome do Evento</Text>
+			<Text style={styles.eventDate}>sexta-feira, 4 de novembro</Text>
+			<View style={styles.form}>
+				<TextInput
+					style={styles.input}
+					placeholder='Nome do participante'
+					placeholderTextColor='#6B6B6B'
+					onChangeText={setParticipantsName}
+					value={participantsName}
+				/>
+				<TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
+
+			<FlatList
+				data={participants}
+				keyExtractor={(item) => item}
+				contentContainerStyle={
+					participants.length > 0 ? null : styles.emptyListContentStyle
+				}
+				renderItem={({ item }) => (
+					<Participants
+						key={item}
+						name={item}
+						onRemove={() => handleParticipantRemove(item)}
+					/>
+				)}
+				showsVerticalScrollIndicator={false}
+				ListEmptyComponent={() => (
+					<Text style={styles.emptyList}>
+						Ninguem chegou no evento ainda? Adicione participantes a sua lista.
+					</Text>
+				)}
+			/>
+		</View>
+	)
+}
